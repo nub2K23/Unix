ec325521af0bd6859a6954a5d6f17ec96220ed8b
nub2K23 <arbindrahul123@gmail.com>
2025-12-02 23:12:53 +0530

    format

--- 24.sh
+++ 24.sh
@@ -3,6 +3,6 @@ Echo “Enter an integer for the bash :”
 read m
 echo “Enter a positive integer for the power:”
 read n
-c=$$($m ** $n))
+c=$$($m ** $n)
 echo “ “
 echo “$m raised to the power of $n is : $c”
