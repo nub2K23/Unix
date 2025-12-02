c4e4bf2b92ef4ef02cb238cb6cfb2b7c16e94059
nub2K23 <arbindrahul123@gmail.com>
2025-12-02 23:50:37 +0530

    fix

--- 21.sh
+++ 21.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-Clear
-Echo –n “Enter a number:”
+!/bin/bash
+clear
+echo -n "Enter a number:"
 read num
-echo “multiplication table for $num:”
+echo "multiplication table for $num:"
 for ((i = 1; i <= 10; i++)); do
-  echo “$num * $i = $((num * i))”
+  echo "$num * $i = $((num * i))"
 done
 
